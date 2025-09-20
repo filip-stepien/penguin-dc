032893aa046e95eb19e32efff62cd73a92b5e3ca
DawOrl <dawid.orlowski2002@gmail.com>
2025-09-20 23:33:32 +0200

    prettier

--- commands/utility/ping.ts
+++ commands/utility/ping.ts
@@ -2,12 +2,10 @@ import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
 import { Command } from '@/lib/types';
 
 const ping: Command = {
-  data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with Pong!'),
-  async execute(interaction: ChatInputCommandInteraction) {
-    await interaction.reply('Pong!');
-  },
+    data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
+    async execute(interaction: ChatInputCommandInteraction) {
+        await interaction.reply('Pong!');
+    }
 };
 
 export default ping;
